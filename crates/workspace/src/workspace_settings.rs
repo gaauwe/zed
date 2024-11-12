a0e246fc6947e715b2778dba995c919f75510a60
Gaauwe Rombouts <gromdroid@gmail.com>
2024-11-12 22:32:05 +0100

    feat: add support for native macOS tabs

--- crates/workspace/src/workspace_settings.rs
+++ crates/workspace/src/workspace_settings.rs
@@ -27,6 +27,7 @@ pub struct WorkspaceSettings {
     pub max_tabs: Option<NonZeroUsize>,
     pub when_closing_with_no_tabs: CloseWindowWhenNoItems,
     pub on_last_window_closed: OnLastWindowClosed,
+    pub use_native_tabs: bool,
 }
 
 #[derive(Copy, Clone, Default, Serialize, Deserialize, JsonSchema)]
@@ -202,6 +203,10 @@ pub struct WorkspaceSettingsContent {
     ///
     /// Default: auto (nothing on macOS, "app quit" otherwise)
     pub on_last_window_closed: Option<OnLastWindowClosed>,
+    /// Whether to use native tabs on macOS.
+    ///
+    /// Default: false
+    pub use_native_tabs: Option<bool>,
 }
 
 #[derive(Deserialize)]
